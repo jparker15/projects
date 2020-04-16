9465104d0cfd5ba9f5c1d3bfe0b956822be2a3b1
Jeremy Parker <jeremyeparker@yahoo.com>
2020-04-16 18:01:43 -0400

    seperate into folders

---
+++ firstAPIs/singleFiles/asyncPractice.js
@@ -0,0 +1,29 @@
+/**
+ * 
+ * Synchronus vs Asynchronous
+ * 
+ * sync code can be looked at as code running online
+ */
+
+ //First Syn Code
+
+
+ // first console log hello world
+
+//  console.log(`hello world \n`);
+
+//  console.log(`hi again \n`);
+
+//  console.log(`hi once more`);
+
+
+
+ // first Async Code 
+
+setTimeout(() =>{
+    console.log('hello world');
+    
+}, 100)
+
+console.log(`hi again`);
+console.log(`howdy`);
