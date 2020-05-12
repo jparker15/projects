8ece8d04bcbb5f4ea5e41a99b08999e6c5889c7c
Jeremy Parker <jeremyeparker@yahoo.com>
2020-05-11 21:34:46 -0400

    xhr requests GET DELETE PATCH for go rest api

---
+++ firstAPIs/goRest/xhr-reqs.js
@@ -0,0 +1,89 @@
+const restKey = "S6M5p6JU9O0GOXmXy4yExjWNoDCUedJL5cfU";
+
+//requestUsers(1)
+
+//deleteUser(1121)
+
+function requestUsers (pageNum) {
+
+    const endpoint = `https://gorest.co.in/public-api/users?access-token=${restKey}&page=${pageNum}`;
+
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET",endpoint,true);
+
+    xhr.onload = () => {
+
+        let parsedData = JSON.parse(xhr.responseText);
+
+       // console.log(parsedData);
+
+        let allUsers = parsedData.result;
+
+       // console.log(allUsers);
+
+        displayUsers(allUsers, parsedData._meta.currentPage);
+        
+        
+    }
+
+    xhr.send();
+
+
+}
+
+function deleteUserReq (userID){
+    const endpoint = `https://gorest.co.in/public-api/users/${userID}?access-token=${restKey}`;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("DELETE",endpoint,true);
+
+    xhr.onload = () => {
+
+        let parsedData = JSON.parse(xhr.responseText);
+        
+        console.log(parsedData);
+    }
+
+    xhr.send();
+
+}
+
+//updateUser(1149)
+
+function updateUserReq (userID){
+
+    let firstName = prompt("First Name","Kelly");
+    let lastName = prompt("Last Name","Clarkson");
+    let email = prompt("EMaiL?", "agentkc@aol.com")
+    const body = JSON.stringify({
+
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        
+
+    });
+
+
+    const endpoint = `https://gorest.co.in/public-api/users/${userID}?access-token=${restKey}`;
+
+    let xhr = new XMLHttpRequest();
+            //PUT can't change entire JSON of this API 
+        xhr.open("PATCH", endpoint, true);
+
+        xhr.onload = () => {
+
+            let parsedData = JSON.parse(xhr.responseText);
+
+            console.log(parsedData);
+            
+
+        }
+
+        xhr.setRequestHeader("Content-Type","application/json");
+
+        xhr.send(body);
+}
