c3bf63bd98558895c6497de838dbfdfefb65d3f4
Jeremy Parker <jeremyeparker@yahoo.com>
2020-07-13 21:15:35 -0400

    add DELETE and PATCH routes

--- MEN_stack/routes/homeRouter.js
+++ MEN_stack/routes/homeRouter.js
@@ -8,7 +8,7 @@ const User = require("../models/User");
         })
     })
 
- //POST route for Users
+//POST route for Users
 // localhost:4005/post
 //@desc post a new user and store in users collections
 //@path (server path)/post 
@@ -32,7 +32,7 @@ const User = require("../models/User");
         }
     })
 
-     //GET route for Users
+//GET route for Users
 // localhost:4005/users
 //@desc get all users in users collections
 //@path (server path)/users 
@@ -55,6 +55,54 @@ const User = require("../models/User");
         }
     })
 
+//DELETE route for Users
+// localhost:4005/delete/:userID
+//@desc delete a user from user collection
+//@path (server path)/delete/:userID
+//@access public
+
+    router.delete("/delete/:userID",async (req,res) => {
+        try{
+            const delUser = await User.findByIdAndDelete({_id:req.params.userID});
+            res.status(200).json({
+                status:200,
+                message: "user successfully deleted",
+                deleted_user: delUser
+            })
+        }
+        catch(error){
+            res.status(500).json({
+                status:500,
+                message: "error at User DELETE request",
+                error: error.message
+            })
+        }
+    })
+
+//PATCH route for Users
+// localhost:4005/update/:userID
+//@desc update a user that matches query param with ID 
+//@path (server path)/update/:userID
+//@access public
+
+    router.patch("/update/:userID",async (req,res)=> {
+        try{
+            const updateUser = await User.findByIdAndUpdate({_id:req.params.userID});
+            res.status(200).json({
+                status:200,
+                message: "user successfully updated",
+                updated_user: updateUser,
+
+            })
+        }
+        catch(error){
+            res.status(500).json({
+                status:500,
+                message: "error at User DELETE request",
+                error: error.message
+            })
+        }
+    })
 
 
 module.exports = router;
