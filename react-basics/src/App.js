8cbb7a4a7b65c049d56bc882f9675e3019739af5
Jeremy Parker <jeremyeparker@yahoo.com>
2020-09-24 20:44:53 -0400

    add NavBar func/class change div styles

---
+++ react-basics/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBarClass from "./Components/NavBarClass"
+import NavBarFunc from './Components/NavBarFunc';
+
+function App() {
+  return (
+    <div 
+    style={{textAlign:"center"}}>
+      <h1>best test in the west </h1>
+      <NavBarClass/>
+      <NavBarFunc/>
+    </div>
+  );
+}
+
+export default App;
